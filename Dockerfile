3eadb12dab9bbaa7114bfdd4dd6d78b6e3448c74
Kevin Moore <kevin@sbotnas.io>
2023-11-16 22:04:44 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,64 @@
+FROM --platform=linux/386 jlesage/baseimage-gui:alpine-3.18-v4
+
+COPY ./assets/nolf1_icon.png /tmp/nolf1_icon.png
+
+RUN \ 
+	set-cont-env HOME "/container/.wine/drive_c/nolf" && \
+	set-cont-env APP_NAME "NOLF Dedicated Server" && \
+	set-cont-env APP_VERSION "1.3" && \
+	set-cont-env DOCKER_IMAGE_VERSION "alpine-3.18-v4" && \
+	set-cont-env DOCKER_IMAGE_PLATFORM "linux/386" && \
+    	APP_ICON_URL=/tmp/nolf1_icon.png && \
+    	install_app_icon.sh "$APP_ICON_URL"
+
+ENV \
+ 	WINEDEBUG="-all" \
+ 	WINEARCH="win32" \
+ 	WINEPREFIX="/container/.wine" \
+ 	WINEDLLOVERRIDES="mscoree,mshtml=" \
+	SERVER_NAME="A NOLF Docker Server" \
+	SERVER_PASSWORD="" \
+	SERVER_PORT="27888" \
+	MAX_PLAYERS="8" \
+	DISABLE_WIZARD="False" \
+	CUSTOM_REZ="" \
+	GAMETYPE="2" \
+#	ASSAULT_MAPS="" \
+#	DEATHMATCH_MAPS="" \
+#	PERSIST_MAPLIST="False" \
+	ADDITIONAL_ARGS=""
+	
+RUN \
+	add-pkg wine && \
+	add-pkg --virtual build xvfb-run cabextract wget && \
+	wget -q -nc --show-progress --progress=bar:force:noscroll --no-hsts -O /tmp/winetricks --user-agent=Mozilla --content-disposition -E -c "https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks" && \
+	chmod +x /tmp/winetricks && \
+	mv /tmp/winetricks /usr/local/bin && \
+	mkdir 760 /container && \
+        xvfb-run /usr/local/bin/winetricks -q mfc42 msvcirt vb6run vcrun6 vcrun2019 && \
+    	mv /container/.wine/drive_c/users/root /container/.wine/drive_c/users/app && \
+    	sed-patch 's|\\root\\|\\app\\|g' /container/.wine/user.reg  && \
+    	sed-patch 's|\\root\\|\\app\\|g' /container/.wine/userdef.reg && \
+	wineboot -u && \
+	echo 'disable' > $WINEPREFIX/.update-timestamp && \
+	chown -R 1000:1000 /container && \
+        del-pkg build
+
+WORKDIR /container/.wine/drive_c/nolf
+
+COPY nolf_startup.sh /startapp.sh
+COPY --chown=1000:1000 ./gamefiles /container/.wine/drive_c/nolf
+
+WORKDIR /container/.wine/drive_c/nolf
+
+EXPOSE 27888-27889/udp
+EXPOSE 5800
+
+LABEL \
+	org.opencontainers.image.authors="Kevin Moore" \
+	org.opencontainers.image.title="NOLF Dedicated Server" \
+	org.opencontainers.image.description="Docker container for running a NOLF Dedicated Server" \
+	org.opencontainers.image.source=https://github.com/MisterCalvin/nolf-server \
+	org.opencontainers.image.version="1.0" \
+	org.opencontainers.image.licenses=MIT
+
---
+++ nolf_startup.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+## File: NOLF Dedicated Server Docker Script - nolf_startup.sh
+## Author: Kevin Moore <admin@sbotnas.io>
+## Date: 2023/11/13
+## License: MIT License 
+GAMEDIR=/container/.wine/drive_c/nolf
+
+cd /container/.wine/drive_c/nolf
+
+# Clear our startup.txt file starting from MODENIZER.rez down
+sed -i '11,$d' "$GAMEDIR/startup.txt"
+
+if [[ ! -z "$CUSTOM_REZ" ]]; then
+	if [[ "$DISABLE_WIZARD" == "[Tt]rue" ]]; then
+	
+		# Rez number for MODERNIZER.rez in startup.txt	
+		i=8
+
+		for f in $CUSTOM_REZ; do
+			if [[ "$(grep $(echo $f | tr '[a-z]' '[A-Z]') "$GAMEDIR/startup.txt")" ]]; then
+               			echo "Duplicate rez found, skipping $f."
+                 		continue
+         		else
+        	        	echo "Rez$(( i=$i+1 )) = \"custom/$f.rez\"" >> "$GAMEDIR/startup.txt"
+				echo "Adding $f.rez"
+			fi
+        	done
+	else
+		# Disable custom rez add for people who only want to use the wizard to manage their server
+		echo "Server Wizard active, skipping custom rez add"
+	fi
+fi
+
+# Logic for handling adding/removing maps to NetHost.txt via docker-compose with included map persistence
+# Mostly works with some bugs, use at your own risk
+# Disabled for RC1 as it is too cumbersome syncing changes between Server GUI Wizard & Docker compose/run commands, may revisit in future
+: <<'END_MAP_LOGIC'
+case $GAMETYPE in
+        1) # H.A.R.M vs. UNITY
+        map_path="Worlds\Multi\AssaultMap"
+        levels="$ASSAULT_MAPS"
+        hoststring="NetCALevel"
+	netnum="NetCANumLevels"
+        ;;
+
+        2|*) # DeathMatch
+        map_path="Worlds\Multi\DeathMatch"
+        levels=$DEATHMATCH_MAPS
+        hoststring="NetLevel"
+	netnum="NetNumLevels"
+        ;;
+esac
+
+if [[ "$PERSIST_MAPLIST" == "[Tt]rue" ]]; then
+        lvlcnt=$( cat "$GAMEDIR/NetHost.txt" | grep $hoststring | wc -l)
+        reqlvlcnt=$(echo $levels | wc -w)
+
+        if [[ $(( $lvlcnt + $reqlvlcnt )) -gt 32 ]]; then
+                echo "Caution, there are more than 32 maps in rotation, server may become unstable or crash."
+        fi
+else
+        sed -i "/$hoststring/d" "$GAMEDIR/NetHost.txt"
+        lvlcnt=0
+fi
+
+for i in $levels; do
+        if [[ "$(grep $(echo $i | tr '[a-z]' '[A-Z]') "$GAMEDIR/NetHost.txt")" ]]; then
+                echo "Duplicate found, skipping $i."
+                continue
+        else
+                echo "$hoststring$lvlcnt = \"$map_path\\$(echo $i | tr '[a-z]' '[A-Z]')\"" >> "$GAMEDIR/NetHost.txt"
+                lvlcnt=$(( $lvlcnt + 1 ))
+        fi
+done
+
+sed -i "s/$netnum.*/$netnum = $(( $lvlcnt )).000000f/g" "$GAMEDIR/NetHost.txt"
+END_MAP_LOGIC
+
+if [[ ! -z "$SERVER_PASSWORD" ]]; then
+        PASSWORD_ENABLED="1.0"
+	sed -i -e "s/NetPassword.*/NetPassword = \"$SERVER_PASSWORD\"/g; 
+		s/NetUsePassword.*/NetUsePassword = 1.000000f/g" "$GAMEDIR/NetHost.txt"
+else
+        PASSWORD_ENABLED="0.0"
+	sed -i -e "s/NetPassword.*/NetPassword = \"\"/g; 
+		s/NetUsePassword.*/NetUsePassword = 0.000000f/g" "$GAMEDIR/NetHost.txt"
+fi
+
+if [[ "$DISABLE_WIZARD" = "[Tt]rue" ]]; then
+        DISABLE_WIZARD="-nowiz"
+else
+        DISABLE_WIZARD=""
+fi
+
+exec wine NolfServ.exe ${DISABLE_WIZARD:-} -NetSessionName "${SERVER_NAME:-"A NOLF Docker Server"}" -NetGameType ${GAMETYPE:=2} ${PASSWORD_ENABLED:+"-NetUsePassword $PASSWORD_ENABLED"} ${SERVER_PASSWORD:+"-NetPassword $SERVER_PASSWORD"} -NetMaxPlayers ${MAX_PLAYERS:-8} -NetPort ${SERVER_PORT:-27888} ${ADDITIONAL_ARGS:-} 
